84a48a1e9ee91176b1330c69ef7e18fe9b897de0
Mohammad Harith <15016508+harixth@users.noreply.github.com>
2022-08-21 18:39:29 +0800

    change to local kafka

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,7 @@ async function bootstrap() {
     transport: Transport.KAFKA,
     options: {
       client: {
-        brokers: ['pkc-ldvr1.asia-southeast1.gcp.confluent.cloud:9092'],
-        ssl: true,
-        sasl: {
-          mechanism: 'plain',
-          username: 'S3MOJJUU4ITVXWHV',
-          password:
-            'sxEoS85PNh6R7K59gl9VViNtgT1oCecktVGn7sx91cq4u2Dy5YpWiuzUswEhXXLw',
-        },
+        brokers: ['kafka:9093'],
       },
       consumer: {
         groupId: 'pricing-consumer',
--- src/price/price.module.ts
+++ src/price/price.module.ts
@@ -11,14 +11,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.KAFKA,
         options: {
           client: {
-            brokers: ['pkc-ldvr1.asia-southeast1.gcp.confluent.cloud:9092'],
-            ssl: true,
-            sasl: {
-              mechanism: 'plain',
-              username: 'S3MOJJUU4ITVXWHV',
-              password:
-                'sxEoS85PNh6R7K59gl9VViNtgT1oCecktVGn7sx91cq4u2Dy5YpWiuzUswEhXXLw',
-            },
+            brokers: ['kafka:9092'],
           },
           consumer: {
             groupId: 'trading-consumer',
